8b585be12a5f873ee7128f3c46a02ab12d0ea6c8
CLayton Wakefield <acidduckling@gmail.com>
2018-05-30 13:35:08 +1000

    Login and Displaying the current user

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
   isNavCollapsed = false;
+  user: firebase.User;
 
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      this.user = user;
+    });
+  }
 
-  ngOnInit() {}
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
