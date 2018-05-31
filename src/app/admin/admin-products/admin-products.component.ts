51b86a52a4653a697487723b5c9679afa2322565
CLayton Wakefield <acidduckling@gmail.com>
2018-05-31 11:49:39 +1000

    Displaying the list of products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../product.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products$: FirebaseListObservable<any[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll();
   }
 
+  ngOnInit() {}
 }
