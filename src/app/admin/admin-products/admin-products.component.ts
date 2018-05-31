34248d5b24393d4b630beca44598fcf7c77c2537
CLayton Wakefield <acidduckling@gmail.com>
2018-05-31 18:30:41 +1000

    Filtering and sorting with the DataTable

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
+import { DataTableResource } from 'angular5-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -9,16 +11,28 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnDestroy {
-  products: { title: string }[];
-  filteredProducts: any[];
+  itemCount: number;
+  items: Product[] = [];
+  products: Product[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
 
   constructor(private productService: ProductService) {
-    this.subscription = this.productService
-      .getAll()
-      .subscribe(
-        products => (this.filteredProducts = this.products = products)
-      );
+    this.subscription = this.productService.getAll().subscribe(products => {
+      this.products = products;
+
+      this.initialiseTable(products);
+    });
+  }
+
+  private initialiseTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource
+      .query({
+        offset: 0
+      })
+      .then(items => (this.items = items));
+    this.tableResource.count().then(count => (this.itemCount = count));
   }
 
   ngOnDestroy() {
@@ -26,10 +40,17 @@ export class AdminProductsComponent implements OnDestroy {
   }
 
   filter(query: string) {
-    this.filteredProducts = query
+    const filteredProducts = query
       ? this.products.filter(p =>
           p.title.toLowerCase().includes(query.toLowerCase())
         )
       : this.products;
+
+    this.initialiseTable(filteredProducts);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+    this.tableResource.query(params).then(items => (this.items = items));
   }
 }
