c40245bd9ee219a5955de4a1146d83b0872a8f70
CLayton Wakefield <acidduckling@gmail.com>
2018-06-02 10:01:27 +1000

    Displaying number of items in shopping cart

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,21 +2,38 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
 import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnDestroy {
+export class NavComponent implements OnInit, OnDestroy {
   isNavCollapsed = false;
   appUser: AppUser;
+  shoppingCartItemCount: number;
   appUserSubscription: Subscription;
+  cartSubscription: Subscription;
 
-  constructor(private auth: AuthService) {
-    this.appUserSubscription = auth.appUser$.subscribe(
+  constructor(
+    private auth: AuthService,
+    private cartService: ShoppingCartService
+  ) {}
+
+  async ngOnInit() {
+    this.appUserSubscription = this.auth.appUser$.subscribe(
       appUser => (this.appUser = appUser)
     );
+
+    const cart$ = await this.cartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (const productId in cart.items) {
+        if (cart.items.hasOwnProperty(productId))
+          this.shoppingCartItemCount += cart.items[productId].quantity;
+      }
+    });
   }
 
   logout() {
@@ -25,5 +42,6 @@ export class NavComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.appUserSubscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
   }
 }
