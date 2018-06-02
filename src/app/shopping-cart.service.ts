0ff62a97c54983014618529a0bc5cf8a3560d1e4
CLayton Wakefield <acidduckling@gmail.com>
2018-06-02 13:15:00 +1000

    Clearing the shopping cart

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -12,30 +12,11 @@ import { Observable } from 'rxjs/Observable';
 export class ShoppingCartService {
   constructor(private db: AngularFireDatabase) {}
 
-  private create() {
-    return this.db
-      .list('/shopping-carts')
-      .push({ dateCreated: new Date().getTime() });
-  }
-
   async getCart(): Promise<Observable<ShoppingCart>> {
     const cartId = await this.getOrCreateCartId();
     return this.db
       .object('/shopping-carts/' + cartId)
       .map(cart => new ShoppingCart(cart.items));
-  }
-
-  private getItem(cartId: string, productId: string) {
-    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
-  }
-
-  private async getOrCreateCartId(): Promise<string> {
-    const cartId = localStorage.getItem('cartId');
-    if (cartId) return cartId;
-
-    const result = await this.create();
-    localStorage.setItem('cartId', result.key);
-    return result.key;
   }
 
   async addToCart(product: Product) {
@@ -58,5 +39,29 @@ export class ShoppingCartService {
         quantity: (item.quantity || 0) + change
       });
     });
+  }
+
+  async clearCart() {
+    const cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
+  private create() {
+    return this.db
+      .list('/shopping-carts')
+      .push({ dateCreated: new Date().getTime() });
+  }
+
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  private async getOrCreateCartId(): Promise<string> {
+    const cartId = localStorage.getItem('cartId');
+    if (cartId) return cartId;
+
+    const result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
   }
 }
