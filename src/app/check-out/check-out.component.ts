2c2a2a4720d99c0419a6f28bd18087403c161e9a
CLayton Wakefield <acidduckling@gmail.com>
2018-06-02 18:15:35 +1000

    Associate order with customer and extract order class

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -3,6 +3,8 @@ import { ShoppingCartService } from '../shopping-cart.service';
 import { ShoppingCart } from '../models/shopping-cart';
 import { Subscription } from 'rxjs/Subscription';
 import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
+import { Order } from '../models/order';
 
 @Component({
   selector: 'app-check-out',
@@ -10,41 +12,33 @@ import { OrderService } from '../order.service';
   styleUrls: ['./check-out.component.css']
 })
 export class CheckOutComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
   shipping: any = {};
   cart: ShoppingCart;
 
   constructor(
     private cartService: ShoppingCartService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private authService: AuthService
   ) {}
 
   async ngOnInit() {
     const cart$ = await this.cartService.getCart();
-    this.subscription = cart$.subscribe(cart => (this.cart = cart));
+    this.cartSubscription = cart$.subscribe(cart => (this.cart = cart));
+    this.userSubscription = this.authService.user$.subscribe(
+      user => (this.userId = user.uid)
+    );
   }
 
   placeOrder() {
-    const order = {
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        };
-      })
-    };
-
+    const order = new Order(this.userId, this.shipping, this.cart);
     this.orderService.storeOrder(order);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 }
---
+++ src/app/models/order.ts
@@ -0,0 +1,27 @@
+import { Shipping } from './shipping';
+import { ShoppingCart } from './shopping-cart';
+
+export class Order {
+  datePlaced: number;
+  items: any[];
+
+  constructor(
+    public userId: string,
+    public shipping: Shipping,
+    cart: ShoppingCart
+  ) {
+    this.datePlaced = new Date().getTime();
+
+    this.items = cart.items.map(i => {
+      return {
+        product: {
+          title: i.title,
+          imageUrl: i.imageUrl,
+          price: i.price
+        },
+        quantity: i.quantity,
+        totalPrice: i.totalPrice
+      };
+    });
+  }
+}
