f7b904b9153fedc101a867c89e5fabcb2109b332
CLayton Wakefield <acidduckling@gmail.com>
2018-06-04 10:02:43 +1000

    Add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { SharedModule } from 'shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([])],
-  declarations: [NavComponent, HomeComponent, LoginComponent],
-  exports: [NavComponent]
+  declarations: [NavComponent, HomeComponent, LoginComponent, FooterComponent],
+  exports: [NavComponent, FooterComponent]
 })
 export class CoreModule {}
